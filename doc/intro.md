c5f724ea411d0398bf91acd01b70a368a24a0c6d
LudoTheHUN <ludoart@yahoo.com>
2013-04-28 14:54:23 +0200

    first few todos

--- doc/intro.md
+++ doc/intro.md
@@ -1,3 +1,5 @@
 # Introduction to adatx
 
+Writes clojure for you, so you just write the failing tests...
+
 TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- src/adatx/core.clj
+++ src/adatx/core.clj
@@ -1,6 +1,20 @@
 (ns adatx.core)
 
+
+
+;;TODO
+;inspect correct arrities
+;create known function store
+;create teacher in-out pairs
+;create valid s-expression builder
+;create safe, execution time aware execute function
+;create brute force search strategy
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+
