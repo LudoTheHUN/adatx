cf191f613d77a37f2912b2249ab78a176fc6fcb9
LudoTheHUN <ludoart@yahoo.com>
2013-05-01 16:10:36 +0200

    do not have safe execution after all

--- src/adatx/core.clj
+++ src/adatx/core.clj
@@ -41,8 +41,6 @@
 
 (deref (takeyourtime 1) 2 :timedout)
 
-(defmacro my_eval [& body]
-  `(future ~@body))
 
 (defmacro my_eval [& body]
   `(try (deref (future
@@ -130,10 +128,6 @@
 ;;(sb '(+ 3 3))
 
 
-(add_timing (my_eval (+ 1 2)))
-(add_timing (my_eval '(do (+ 1 2) )))
-
-
 (def codesinpet '(+ 1 2))
 
 
@@ -182,13 +176,18 @@
 (def codesinpet  '(stackm 10000))  ; shows we are safe to stack overflow
 ;NOTE we are already safe to timeout, so we should never blow up.
 
-(def ans (time (add_timing (my_eval22 codesinpet))))
-ans
+(quote
+  (def ans1 (time (add_timing (my_eval22 codesinpet))))    ;;this blows up clojure??!?!?
+ans1
+  (def ans2 (time (add_timing (my_eval22 '(stackm 1000000)))))
+ans2
+(def ans3 (time (add_timing (my_eval22 '(iterate inc 5)))))
+ans3
+)
+;(sb '(stackm 1000000))
 
-(sb '(stackm 1000000))
-
-(time (add_timing (my_eval21 '(:a 1))))
-(time (add_timing (my_eval21  '(stackm 1000000))))
+;(time (add_timing (my_eval21 '(:a 1))))
+;(time (add_timing (my_eval21  '(stackm 1000000))))
 
 
 
@@ -197,67 +196,39 @@ ans
 (count (flatten codesinpet))
 (count (str codesinpet))
 
-
-
-(defn generate_list [& symbols]
-  (let [len_symbols (count symbols)]
-    len_symbols))
-
-(generate_list 'w)
-              
-
-'(a b [23 h])
-'()
-(cons 'a '(b))
-(cons (vector 'a) '[b [8 9]])
-(vec (cons (vector 'a) '[b [8 9]]))
-
 (def a1 '())
 (def a2 (cons 'let a1))
 (def a3 (cons (vector) a2))
 (def a4 (cons (vec (cons 'a (first a3))) (rest a3)))
 
 
+(quote
 ;;TODO need to track depth... number , pairs for maps and lets (an optimisation)
 ;;take a flat list  with special keywords for starting, ending list,vecs,maps...??
 (hash-map 'a 'b)
-
-(add_timing (my_eval '(hash-map :3 2 5)))
-
-(add_timing (my_eval '(+ 1 2)))
-  
+(add_timing (my_eval21 '(hash-map :3 2 5)))
+(add_timing (my_eval21 '(+ 1 2)))
 (conj {:d (+ 1 2)} {:foo 2 :boo 3})
-
-(macroexpand-1 '(my_eval (+ 1 2)))
-
+(macroexpand-1 '(my_eval21 (+ 1 2)))
 (let [dothis '(+ 12 3)]
-  (my_eval dothis))
-
+  (my_eval21 dothis))
 (let [dothis '(- 12 3)]
   (my_eval dothis))
-
 (let [dothis {:ex '(- 12 3)}]
   (my_eval (:ex dothis)))
-
 (let [dothis {:ex '(/ 12 0)}]
   (my_eval (:ex dothis)))
-
 (let [a1     3
       dothis '(+ 12 a1)]
  (my_eval dothis))
-
 (my_eval
 (let [a1     3
       dothis '(+ 12 4)]
   dothis
   ))
-
 (my_eval 3)
-
 (my_eval (+ 12 3))
-
 (eval '(+ 12 3))
-
 (defn safederef [future]
 (try
    {:answer (deref future 
@@ -266,45 +237,69 @@ ans
     }
    (catch Exception e  
          {:error 1 :errormsg e}   )))
-
 (safederef (my_eval (do (Thread/sleep 10)  (+ 1 2))))
-
 (macroexpand-1 (my_eval (do (+ 1 2))))
 
 
+)  ;end quote
 
 
 ;;-----------------------  genlist
 
 (def spec '(:l 1 2))
 
-(defn genprog-1step [partial spec depth]
+(defn genprog [partial spec depth]
   (println "partial:" partial " spec:" spec " depth:"depth)
-  (let [specf (first spec)
-        depthf (first depth)]
+  (let [specf        (first spec)
+        depthf       (first depth)
+        spec_past_ld (drop-while #(not (= % :ld)) spec)
+        spec_pre_ld  (take-while #(not (= % :ld)) spec)]
     (cond 
+    ;  (= specf :ld)
+    ;       nil
       (= depthf :l)
-           (cons (genprog-1step  (first partial) spec (rest depth)) (rest partial))
+           (cons (genprog  (first partial) spec (rest depth)) (rest partial))
       (= specf :l)
-           (genprog-1step (cons (list) partial) (rest spec) (cons :l depth))
+           (genprog
+             (genprog (cons (list) partial) (rest spec) (cons :l depth))   ;;ok from the right
+             (rest spec_past_ld)
+             depth)      
       (not (= specf nil))
-           (genprog-1step (cons specf partial)  (rest spec) depth)
+           (genprog (cons specf partial)  (rest spec) depth)
       :else
         partial)
   ))
 
+;;;   :l make a list   :ld  list down  
 
-(genprog-1step 3 spec nil)
-(genprog-1step '(3) spec nil)
-(genprog-1step '(3 4) spec nil)
-
-(genprog-1step '(:a) '(:l 1 :l 2) nil)
-
-(genprog-1step '((:b ):a) '(:l 1 :l 2) '(:l))
-
-(genprog-1step nil '(1 :l 2) nil)
-(genprog-1step nil '(1 3 :l 2 4 :l 5) nil)
+;;Test worthy
+(genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 ) nil)   ; '(9 8 (7 6 5) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l 5 6 :l  7 :ld 8 9 :ld 10 ) nil)   ; '(10 (9 8 (7) 6 5) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 ) nil)   ; '(9 8 (7 6 5) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l 5 6 7 8 9 ) nil)       ; '((9 8 7 6 5) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l :l :l 5 6 7 :ld 8 9 ) nil)  ;; '(((9 8 (7 6 5))) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l :l :l 5 6 7 :ld 8 9 :ld 10 11) nil)  ;; '(( 10 11 (9 8 (7 6 5))) 4 3 2 1)
 
 
+(genprog '(:a) '(:l 1 :l 2) nil)
+
+(second '(1))
+
+(split-at 4 [1 1 1 1 1 1 1 1])
+(drop-while #(not (= % :ld)) '(1 2 3 :ld  4 5 6 :ld 7 8 ))
+(drop-while #(not (= % :ld)) '(1 2 3  4 5 6 7 8 ))
+(take-while #(not (= % :ld)) '(1 2 3 :ld 4 5 6 7 8 ))
+
+(genprog '((:b ):a) '(:l 1 :l 2) '(:l))
+
+(genprog nil '(1 :l 2) nil)
+(genprog nil '(1 3 :l 2 4 :l #{5 4 3} '2 "foo" :goo {:key 'val} :l [3 3 5] ) nil)
+(genprog nil '(1 2 3 4 :l 4 5 6 ) nil)
+
+
+
+(= #{5 4 3} #{3 4 5})
+
+'((({:key (quote val)} :goo "foo" (quote 2) 5) 4 2) 3 1)
 (cons 'nil '(a b 1))
 (list)
