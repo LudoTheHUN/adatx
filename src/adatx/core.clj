8e04256d43d828f743e4582a24ff6cde247c622a
LudoTheHUN <ludoart@yahoo.com>
2013-05-02 14:51:49 +0200

    basic genprog works, needs array capability

--- src/adatx/core.clj
+++ src/adatx/core.clj
@@ -116,42 +116,97 @@
 
 (def spec '(:l 1 2))
 
+
+(defn pairoff-pre [list accum match matchd depth]
+  (cond 
+     (and (empty? depth) (not (empty? accum)))
+       accum
+     (= (first list) match)
+       (do ;(println "depth up")
+         (pairoff-pre (rest list) (cons (first list) accum) match matchd (cons match depth)))
+     ;(and (= (first list) matchd) (not (empty? depth)))
+     (= (first list) matchd)
+       (do ;(println "depth down")
+         (pairoff-pre (rest list) (cons (first list) accum) match matchd (rest depth)))
+     (and (not (empty? depth)) (not (empty? list)))
+       (do ;(println "non nil depth")
+         (pairoff-pre (rest list) (cons (first list) accum) match matchd depth))
+     (empty? depth)
+        accum
+  :else
+    accum
+  ))
+
+(defn r-pairoff-pre [list  match matchd]
+ "looks ahead and returns list until match and matchd are all paired off."
+  (reverse (pairoff-pre list nil match matchd nil)))
+
+(defn r-pairoff-post [list  match matchd]
+ "looks ahead and returns list until match and matchd are all paired off."
+    (let [len (count (pairoff-pre list nil match matchd nil))]
+      (drop len list)))
+     
+(r-pairoff-pre '() :l :ld)
+(r-pairoff-pre nil :l :ld)
+(r-pairoff-pre '(1 2 3 :l 4 5 6 7 8)  :l :ld)      ;-> '()
+(r-pairoff-pre '(1 2 3 :l 4 5 :ld 6 7 8)  :l :ld)  ;-> '()
+(r-pairoff-pre '(:l 4 5 :ld 6 7 8)        :l :ld)  ;-> '(:l 4 5 :ld)
+(r-pairoff-pre '(:l 4 :l 5 :ld 6 :ld 7 8) :l :ld)  ;-> '(:l 4 :l 5 :ld 6 :ld)
+(r-pairoff-pre '(:l 4 5 :ld 6 :ld 7 8) :l :ld)     ;-> '(:l 4 5 :ld)
+(r-pairoff-pre '(:l 4 5 :l 6 :ld 7 8) :l :ld)      ; -> (:l 4 5 :l 6 :ld 7 8)
+(r-pairoff-pre '(:l 4 5 :l :l 6 :ld :ld :ld 7 8) :l :ld)     ; (:l 4 5 :l :l 6 :ld :ld :ld)
+(r-pairoff-pre '(:l :l :l :l 6 :ld :ld :ld :ld 7 8) :l :ld)  ; (:l :l :l :l 6 :ld :ld :ld :ld)
+(r-pairoff-pre '(:l :l :l 6 :ld :ld :ld :l 7 8) :l :ld)      ;-> (:l :l :l 6 :ld :ld :ld)
+(r-pairoff-pre '(:l :ld 2 :l 6 :ld :ld :ld :l 7 8) :l :ld)   ;->  (:l :ld)
+(r-pairoff-pre '(:l :ld :ld :ld :ld :l 7 8) :l :ld)          ;->  (:l :ld)
+(r-pairoff-pre '(:l 1 2 :l 3 :ld 4 5 :l 6 :ld 7 :ld 8 9) :l :ld)   ;-> (:l 1 2 :l 3 :ld 4 5 :l 6 :ld 7 :ld)
+(r-pairoff-pre '(:l 1 2 :l 3 :ld :l 6 :ld 7 :ld 8 9) :l :ld)       ;-> (:l 1 2 :l 3 :ld :l 6 :ld 7 :ld)
+(r-pairoff-pre '(:l 1 2 :l 3 :ld :l 6 :ld :ld 8 9) :l :ld)         ;-> (:l 1 2 :l 3 :ld :l 6 :ld :ld)
+(r-pairoff-pre '(:l 1 2 :l 3 :l :ld :l :ld 6 :ld :ld 8 9) :l :ld)  ;-> (:l 1 2 :l 3 :l :ld :l :ld 6 :ld :ld)
+(r-pairoff-pre '(:l 1 2 :l 3 :l :ld :l :ld 6 :ld :ld :ld 8 9) :l :ld)  ;-> (:l 1 2 :l 3 :l :ld :l :ld 6 :ld :ld)
+
+(time (r-pairoff-pre '(:l 1 2 :l 3 :l :ld :l :ld 6 :ld :ld :ld 8 9) :l :ld))
+
+
+(r-pairoff-post '() :l :ld)
+(r-pairoff-post nil :l :ld)
+(r-pairoff-post nil :l :ld)
+(r-pairoff-post '(:l 1 2 :l 3 :l :ld :l :ld 6 :ld :ld :ld 8 9) :l :ld)
+(r-pairoff-post '(:l 4 5 :ld 6 7 8)        :l :ld)
+(r-pairoff-post '(:l :ld 2 :l 6 :ld :ld :ld :l 7 8) :l :ld)
+
+
 (defn genprog [partial spec depth]
+  "WIP works, needs to do much to make it array literal aware"
   (println "partial:" partial " spec:" spec " depth:"depth)
   (let [specf        (first spec)
         depthf       (first depth)
-        spec_past_ld (drop-while #(not (= % :ld)) spec)
-        spec_pre_ld  (take-while #(not (= % :ld)) spec)]
+        spec_pre_ld  (r-pairoff-pre spec :l :ld)
+        spec_past_ld (let [len (count spec_pre_ld)] (drop len spec))
+        second_spec  (second spec)]
     (cond 
-    ;  (= specf :ld)
-    ;       nil
       (= depthf :l)
            (cons (genprog  (first partial) spec (rest depth)) (rest partial))
-      (= specf :l)
-           (genprog
-             (genprog (cons (list) partial) (rest spec) (cons :l depth))   ;;ok from the right
-             (rest spec_past_ld)
-             depth)      
-      (not (= specf nil))
+      (= specf :l)     ;;starting a new list
+           (genprog    ;;work past return to the same depth level
+             (genprog (cons (list) partial) (rest spec_pre_ld) (cons :l depth))   ;;instructions with which to fill the inner list
+             spec_past_ld
+             depth)
+      (and (not (= specf nil)) (not (= specf :ld)))    ;; the sale level add a symbol
            (genprog (cons specf partial)  (rest spec) depth)
       :else
         partial)
   ))
 
-;;;   :l make a list   :ld  list down  
-
-;;Test worthy
-(quote 
 (genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 ) nil)   ; '(9 8 (7 6 5) 4 3 2 1)
-(genprog nil '(1 2 3 4 :l 5 6 :l  7 :ld 8 9 :ld 10 ) nil)   ; '(10 (9 8 (7) 6 5) 4 3 2 1)
-(genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 ) nil)   ; '(9 8 (7 6 5) 4 3 2 1)
-(genprog nil '(1 2 3 4 :l 5 6 7 8 9 ) nil)       ; '((9 8 7 6 5) 4 3 2 1)
-(genprog nil '(1 2 3 4 :l :l :l 5 6 7 :ld 8 9 ) nil)  ;; '(((9 8 (7 6 5))) 4 3 2 1)
-(genprog nil '(1 2 3 4 :l :l :l 5 6 7 :ld 8 9 :ld 10 11) nil)  ;; '(( 10 11 (9 8 (7 6 5))) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l 5 6 :l  7 :ld 8 9 :ld 10 11 ) nil)   ; '(10 (9 8 (7) 6 5) 4 3 2 1)
+(genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 :ld ) nil)   ; '(9 8 (7 6 5) 4 3 2 1)
 
-)
+(genprog nil '(1 2 3 4 :l :l 6 :l  7 :ld 8 9 :l :ld 10 :ld :ld 11 :ld :ld) nil)
 
-;;Defining sb again at the end so we do not lose all out defined functions above with each call to sb
+(genprog nil '(1 2 3 4 :l :l 6 :l  7 :ld 8 9 :ld :ld 10 :l :l :l :ld :ld 11 :ld :ld) nil)
+
+
+
 (def sb (sandbox tester :timeout 50 :namespace 'adatx.core))   ;;; NOTE sb will snapshot the 'adatx.core namespace as it is at the time we come here, it will be reset to this state with each call to sb
-
 
--- test/adatx/core_test.clj
+++ test/adatx/core_test.clj
@@ -2,6 +2,33 @@
   (:use clojure.test
         adatx.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
+
+
+
+(deftest genprog-test
+  (testing "genprog for correct behaviour"
+    (is (= 
+          (genprog nil '(1 2 3 4 :l 5 6 :l  7 :ld 8 9 :ld 10 ) nil nil)   
+          '(10 (9 8 (7) 6 5) 4 3 2 1)
+          ))
+ (quote   (is (=
+          (genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 ) nil nil)
+          '(9 8 (7 6 5) 4 3 2 1)
+          ))
+    (is (= 
+          (genprog nil '(1 2 3 4 :l 5 6 7 :ld 8 9 ) nil nil)
+          '(9 8 (7 6 5) 4 3 2 1)
+          ))
+    (is (= 
+          (genprog nil '(1 2 3 4 :l :l :l 5 6 7 :ld 8 9 ) nil nil)  
+          '(((9 8 (7 6 5))) 4 3 2 1)
+          ))
+    (is (=
+          (genprog nil '(1 2 3 4 :l :l :l 5 6 7 :ld 8 9 :ld 10 11) nil nil) 
+          '(( 10 11 (9 8 (7 6 5))) 4 3 2 1)
+          ))
+  )
+))
