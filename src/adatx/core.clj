1a9a8c1e4f0167fac7e971e3a6c6cc713f73dcb1
LudoTheHUN <ludoart@yahoo.com>
2013-05-11 17:44:48 +0200

    batched evals under in a sandbox

--- src/adatx/core.clj
+++ src/adatx/core.clj
@@ -17,6 +17,12 @@
 
 ;Consider a spec comparator function...
 
+;;TODO Identify all the critical errors that only sb can survive
+  ;;Work out the sandboxing strategy for batching evaluations, everything always in a sandbox, but with local try catches for speed
+  ;;Work out how to fall back to indivual sandboxing of each candidate if a batch fails.
+  ;;Know what a critical failour of a batch looks like so that a fall back can be started.
+  ;;look to parallelise the batching +fallback execution... should be always fully loaded.
+
 ;reseach core.logic to see if logic programs count be found this way
 ;;DONE Setting up sandbox in clojail
 
@@ -80,6 +86,19 @@
 (defn my_eval22 [body]     ;;function version here also works, but breaks the :quoted key, which is not important
   (let [fut (future
                {:eval-sb   (sb body)  ;sandboxed evaluation
+                :expr    body 
+                :quoted 'body})
+        ans (try (deref fut
+                     50 {:timeout 50 :expr    body :quoted 'body :error 1})
+                  (catch Exception e {:error 1 :errormsg e     :expr    body :quoted 'body}))
+         ]
+     ;(future-cancel fut)
+     ans
+   ))
+
+(defn my_eval22e [body]     ;;function version here also works, but breaks the :quoted key, which is not important
+  (let [fut (future
+               {:eval-sb   (eval body)  ;sandboxed evaluation
                 :expr    body 
                 :quoted 'body})
         ans (try (deref fut
@@ -369,7 +388,14 @@
  (postwalk-replace {::prog prog} holder) )
 
 (defn exemplar-test-list [prog-holder prog x-in]
-    (list (prog_wrap prog-holder prog) x-in))
+    (cons (prog_wrap prog-holder prog) x-in))
+
+
+;;  (cons (prog_wrap prog-holder '(+ x 10) ) '(10))
+;;  (eval (cons (prog_wrap prog-holder '(+ x 10) ) '(10)))
+;;  (eval ((fn ([x] (+ x 10)) ([x y] (+ x y 10))) 10 5))
+;;  (eval ((fn foo ([x] (+ x 10 (foo 0))) ([x y] (+ x y 10))) 10 5))
+
 
 (defn exemplar-in [prog-holder prog x-in y-out]
   "WIP evaluates the prog against the x-in data
@@ -384,32 +410,101 @@
     y-ans))
 
 (def prog-holder
-  "example of the simples prog holder"
-   '(fn [x] ::prog))
+  "a prog holder that will take longer then needed to find a specific arity function, do not use.
+   NOTE finding veriadic functions is not supported, but would require small alterations to prog_wrap, or a rethink of the prog-holder idea."
+   '(fn ::prog))
 
 (def prog-holder
- "prog-holder that embeds the search within an exisitng AST"
+  "a prog holder that will eventually find a potentailly recursive function if myfname symbal is in the keylist and symlookup"
+   '(fn myfname ::prog))
+
+(def prog-holder
+ "prog-holder that embeds the search within an exisitng AST, here adds x to whatever the function spec the ::prog is"
    '(fn [x] (+ x ::prog)))
 
+(def prog-holder
+  "example of the simples two arity prog holder, x and y should be in keylist and symlookup "
+   '(fn [x y] ::prog))
+
+(def prog-holder
+  "example of the simples single arity prog holder"
+   '(fn [x] ::prog))
 
 (def sb (sandbox tester :timeout 100 :namespace 'adatx.core))
 
 
-(exemplar-in prog-holder '(+ x x) 5 :goo)
+(exemplar-in prog-holder '(+ x x) '(5) :goo)
 
 ;;;;;;;;;;;Prog holder end;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(quote
+  ;;Review of stability under different levels of eval safety.
+  
+  (time (my_eval22 (exemplar-test-list 
+                  '(fn [x] (+ 4 ::prog))
+                   '(+ x x)
+                    '(5))))
+  (time (my_eval2 (exemplar-test-list 
+                  '(fn [x] (+ 4 ::prog))
+                   '(+ x x)
+                    '(5))))
+  (time (my_eval2 (exemplar-test-list 
+                  '(fn forever[x] (+ 4 (forever 4) ::prog))
+                   '(+ x x)
+                    '(5))))
+  ;; this does not blow up my_eval2 due to a type check...
+  (time (my_eval2 (exemplar-test-list 
+                  '(fn forever [x] (+ 4 (iterate inc 1) ::prog))
+                   '(+ x x)
+                    '(5))))
+  ;; But this gives us a GC overhead limit with my_eval2 but is ok with my_eval22 by hitting the timeout.
+  (time (my_eval22 (exemplar-test-list 
+                  '(fn forever [x] (iterate inc 1))
+                   '(+ x x)
+                    '(5))))
+  
+  ;;Plan is to pack 1000 my_eval2 calls into a single my_eval22 call....
+  (time (my_eval22   '(macroexpand-1 '(my_eval2 (exemplar-test-list   ;;"DO NOT RUN, this will blow up even my_eval22"
+                  '(fn forever [x] (iterate inc 1))
+                  ;;  '(fn forever [x] ::prog)
+                   '(+ x x)
+                    '(5))))))
+      ;;This cripples the sandbox  and then returns: #<ExecutionException java.util.concurrent.ExecutionException: java.security.AccessControlException: access denied (java.lang.RuntimePermission createSecurityManager
+      ;; even on other, valid calls.  Kills the repls and even eclipse.
+      
+      
+(defn makemanytests []
+  
+      (map (fn [x] (exemplar-test-list 
+                  '(fn [x] (+ 4 ::prog))
+                   x
+                    '(5)))
+      
+      (genprogs1 100 '(1) '(1 2 :l :ld) {1 '+ 2 'x :l :ld})
+      ))
 
-;  (time (my_eval22 (exemplar-test-list 
-;                   '(fn [x] (+ 4 ::prog))
-;                   '(+ x x)
-;                    5)))
+
+  (defn answers []   (map my_eval22e
+   (makemanytests)))
+ 
+  (def sb2 (sandbox tester :timeout 1000 :namespace 'adatx.core))
+
   
+  (defn answers-safe [] (sb2  '(map my_eval22e
+   (makemanytests))))
   
+  (answers-safe)
+        
   
+(time 
+(pprint (filter #(not (nil? %)) (map :eval-sb (answers-safe))))
+)
+
+ (realized? answer)
+
+)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 
 
@@ -418,13 +513,15 @@
 ;{7 78125, 8 21875}     ;;what would be the saving if we skipped the itterator when depth went below base level?
 ;{7 37472, 8 62528}     ;;with non silly specs
 
-
+;;for these key list was tiny, at (1 2 :l :v :ld)
 ;(time (frequencies (pmap count (take 10000000 (iterate spec_iter_defed  '(1)   )))))
 ;;"Elapsed time: 245897.282269 msecs"  (on the thinkpad)
 ;;{1 4, 2 16, 3 72, 4 336, 5 1600, 6 7712, 7 37472, 8 183104, 9 898432, 10 4422144, 11 4449108} 
 ;(time (frequencies (map count (take 10000000 (iterate spec_iter_defed  '(1)   )))))
 ;;"Elapsed time: 157034.765806 msecs"   (on the thinkpad)
 ;;;{1 4, 2 16, 3 72, 4 336, 5 1600, 6 7712, 7 37472, 8 183104, 9 898432, 10 4422144, 11 4449108}
+
+;;(time (frequencies (map count (take 10000000 (iterate (spec_iterate_f '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 :l :v :ld))  '(1)   )))))
 
 
 
