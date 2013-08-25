b29bfe19529c63b592dc910327d332d7b59fafc0
LudoTheHUN <ludoart@yahoo.com>
2013-08-25 20:38:41 +0100

    in outs application next

--- src/adatx/prob_solve.clj
+++ src/adatx/prob_solve.clj
@@ -41,19 +41,25 @@
 (prog-hold/put-prog-in-prog-holder  prog-holder prog x-ins)
 )
 
-
+(defn WIP-applyin-in-out-pairs []
+  ;;---- need the executors namespace to homd my_eval stuff
+  nil)
+  
+  
 
 (defn prob-solve [prob-holder] 
   (let [prob-holder (preped-prob-holder prob-holder)
         {spec :init-spec  
          keylist :keylist
          symlookup :symlookup
-         prog-holder :prog-holder}     prob-holder
+         prog-holder :prog-holder
+         in-out-pairs :in-out-pairs}     prob-holder
         ]
-    
     ;;;;TODO need a function that will go and apply :in-out-pairs and collect number of correct answers (thus EVAL)
-     
-   (take 3   (map (fn [prog] (prog-hold/put-prog-in-prog-holder prog-holder prog '(1 2 3)))
+   (take 1   (map (fn [prog] 
+                   ;;TODO need to apply ins, test outputs, find correct solutions
+                   ;dont do this (prog-hold/put-prog-in-prog-holder prog-holder prog '(1 2 3)))
+                   (cons (prog-hold/prog_wrap prog-holder prog ) (:in (first in-out-pairs )) ))  ;;WIP ;;first get the ready to run_prog, then add the in
                   (progseq/genprogs-lazy spec keylist symlookup)))
              )
              )
